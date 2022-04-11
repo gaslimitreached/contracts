2a02898951dc74b8978891c1fd66b978fc51bf0f
gaslimitreached <90769841+gaslimitreached@users.noreply.github.com>
2022-04-10 21:32:23 -0500

    fix: lint warnings

--- src/test/utils/mocks/MockERC721.sol
+++ src/test/utils/mocks/MockERC721.sol
@@ -5,9 +5,11 @@ pragma solidity ^0.8.0;
 import "../../../tokens/ERC721/ERC721.sol";
 
 contract MockERC721 is ERC721 {
+    // solhint-disable no-empty-blocks
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
 
     function tokenURI(uint256) public pure virtual override returns (string memory) {}
+    // solhint-enable no-empty-blocks
 
     function mint(address to, uint256 tokenId) public virtual {
         _mint(to, tokenId);
