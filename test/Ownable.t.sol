81b88523f24dc8f744469c3a560744a80a7e7942
gaslimitreached <90769841+gaslimitreached@users.noreply.github.com>
2022-06-21 09:39:21 -0500

    fix: import using proper case

--- test/Ownable.t.sol
+++ test/Ownable.t.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
-import "./Mocks/MockOwnable.sol";
+import "./mocks/MockOwnable.sol";
 
 contract OwnableTest is Test {
     MockOwnable internal mock;
--- test/Ward.t.sol
+++ test/Ward.t.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
-import "./Mocks/MockWard.sol";
+import "./mocks/MockWard.sol";
 
 contract WardTest is Test {
     MockWard internal mock;
